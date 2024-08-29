4d2a1e7d976cbbaec4e072e1dc19c64ad0756d31
dimnov <dimnov1512@gmail.com>
2024-08-30 00:16:48 +0300

    Add QueryClientProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AppRoutes from "./AppRoutes";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
+
 function App() {
-  return <AppRoutes />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AppRoutes />;
+    </QueryClientProvider>
+  );
 }
 
 export default App;
