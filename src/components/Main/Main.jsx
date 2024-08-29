978ffd42b6c9b3c4ea31464e707e5654016bf561
dimnov <dimnov1512@gmail.com>
2024-08-30 00:17:12 +0300

    Add Main component for the Outlet

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+import "./Main.css";
+
+function Main() {
+  return (
+    <main className="main">
+      <Outlet />
+    </main>
+  );
+}
+
+export default Main;
