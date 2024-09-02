6f3d7f64b52e750c90e8ed74db6762e16ead75b4
dimnov <dimnov1512@gmail.com>
2024-09-02 21:30:13 +0300

    Add LoginPopup to the page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,21 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import { useState } from "react";
+import LoginPopup from "./components/LoginPopup/LoginPopup";
 
 function App() {
+  const [showLogin, setShowLogin] = useState(false);
+
   return (
-    <div className="app">
-      <Navbar />
-      <Outlet />
-    </div>
+    <>
+      {showLogin && <LoginPopup setShowLogin={setShowLogin} />}
+      <div className="app">
+        <Navbar setShowLogin={setShowLogin} />
+        <Outlet />
+      </div>
+      <Footer />
+    </>
   );
 }
 
