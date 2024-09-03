8c4dff2d5c66cdbd3608c8373fdf0a4d4a9375d3
dimnov <dimnov1512@gmail.com>
2024-09-03 21:02:43 +0300

    Update Home.jsx

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
 import Header from "../../components/Navbar/Header/Header";
-import "./Home.css";
 import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
 import AppDownload from "../../components/AppDownload/AppDownload";
+import "./Home.css";
 
 function Home() {
   const [category, setCategory] = useState("all");
+
   return (
     <div>
       <Header />
