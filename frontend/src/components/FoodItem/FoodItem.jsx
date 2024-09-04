67239c4f29fb57290a4f5421873a1acd3e9ae650
dimnov <dimnov1512@gmail.com>
2024-09-04 20:34:43 +0300

    bug fix image show

--- frontend/src/components/FoodItem/FoodItem.jsx
+++ frontend/src/components/FoodItem/FoodItem.jsx
@@ -4,14 +4,13 @@ import "./FoodItem.css";
 import { StoreContext } from "../../context/StoreContext";
 
 function FoodItem({ food }) {
-  const { id, name, price, description, image } = food;
-
+  const { id, name, price, description, image_url } = food;
   const { cartItems, addToCart, removeFromCart } = useContext(StoreContext);
 
   return (
     <div className="food-item">
       <div className="food-item-img-container">
-        <img className="food-item-image" src={image} alt="food image" />
+        <img className="food-item-image" src={image_url} alt="food image" />
         {!cartItems[id] ? (
           <img
             className="add"
