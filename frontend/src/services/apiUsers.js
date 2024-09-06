6232dc638a339cc553adeac6d42d66dcd7a8fc71
dimnov <dimnov1512@gmail.com>
2024-09-06 20:17:43 +0300

    create function to get session from supabase

---
+++ frontend/src/services/apiUsers.js
@@ -0,0 +1,15 @@
+import { supabase } from "../lib/supabase";
+
+export async function fetchSession() {
+  const {
+    data: { session },
+    error,
+  } = await supabase.auth.getSession();
+
+  if (error) {
+    throw new Error(error.message);
+  }
+
+  return session;
+}
+
