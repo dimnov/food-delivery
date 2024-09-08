485107c3d1fa14a31204e48db3860e9b3d4382b5
dimnov <dimnov1512@gmail.com>
2024-09-08 23:04:35 +0300

    moved Header files

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
-import Header from "../../components/Navbar/Header/Header";
+import Header from "../../components/Header/Header";
 import FoodDisplay from "../../components/FoodDisplay/FoodDisplay";
 import AppDownload from "../../components/AppDownload/AppDownload";
 import "./Home.css";
